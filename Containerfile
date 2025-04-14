d61c53355660fe30eb478390b3b5dd7aee689f42
focused_xy <elpsycongroo242@gmail.com>
2025-04-14 12:44:10 +0800

    feat: 添加主机代理
    1. 使用了 --network host,使容器共享主机网络
    2. run.sh 需要提供主机代理端口并传递给容器

--- Containerfile
+++ Containerfile
@@ -1,7 +1,19 @@
 FROM docker.educg.net/cg/os-contest:20250226
+ARG HTTP_PORT
+ARG HTTPS_PORT
+ARG http_port
+ARG https_port
+ENV HTTP_PROXY=http://127.0.0.1:${HTTP_PORT}
+ENV HTTPS_PROXY=http://127.0.0.1:${HTTPS_PORT}
+ENV ALL_PROXY=http://127.0.0.1:${HTTP_PORT}
+ENV NO_PROXY=localhost,127.0.0.1,::1
+ENV http_proxy=http://127.0.0.1:${http_port}
+ENV https_proxy=http://127.0.0.1:${https_port}
+ENV all_proxy=http://127.0.0.1:${http_port}
+ENV no_proxy=localhost,127.0.0.1,::1
 RUN apt-get -y update
 RUN apt-get -y install tar git build-essential cmake wget curl dosfstools gdb-multiarch qemu-system-misc scons python3-kconfiglib python3-tqdm python3-requests python3-yaml vim
-COPY toolchains /root
+COPY toolchains /root/toolchains
 RUN bash /root/toolchains/install_ubuntu.sh --gitee
 RUN tar jxvf /root/toolchains/qemu-virt-riscv64/riscv64gc-linux-musleabi_for_x86_64-pc-linux-gnu_latest.tar.bz2 -C /opt
 RUN tar zxvf /root/toolchains/qemu-longarch/loongarch64-musl-gcc-nightly-2025-3-27.tar.gz -C /opt
--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #! /usr/bin/env bash
 
+# 参数校验函数
+usage() {
+    echo "用法: $0 <http_port> [https_port]"
+    echo "示例:"
+    echo "  $0 7890            # http_port=https_port=7890"
+    echo "  $0 8080 8081       # http_port=8080, https_port=8081"
+    exit 1
+}
+
+# 解析命令行参数
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    usage
+fi
+
+http_port="$1"
+https_port="${2:-$http_port}" # 如果第二个参数不存在，复用第一个参数
+
 if [ -n "${BASH_SOURCE[0]}" ]; then
     script_path="${BASH_SOURCE[0]}"
 else
@@ -23,12 +40,23 @@ fi
 
 if ! docker image inspect "$image_name" &>/dev/null; then
     docker build --rm -t "$image_name" \
-        -f "$script_dir/Containerfile" \
+        --network host \
+        --build-arg HTTP_PORT="$http_port" \
+        --build-arg http_port="$http_port" \
+        --build-arg https_port="$https_port" \
+        --build-arg HTTPS_PORT="$https_port" -f "$script_dir/Containerfile" \
         "$script_dir"
 fi
 
 docker run -dit --rm -v .:/code -w /code --name "$container_name" \
-    --privileged "$image_name" bash
+    --network host --privileged "$image_name" bash
 docker exec "$container_name" \
-    make -C testsuits-for-oskernel build-all -j8
-docker attach "$container_name"
+    NPROC=8 make -C testsuits-for-oskernel sdcard
+docker ps -a -f "name=$container_name"
+cat <<EOF
+Use: docker attach $container_name
+     cd machines/qemu-virt-riscv64
+     scons --menuconfig
+     pkgs --update
+     scons -j8
+EOF
