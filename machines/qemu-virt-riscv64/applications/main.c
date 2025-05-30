06c018c7d54c2f6e7e24ec72552ac9c408de8307
Rache <2746324475@qq.com>
2025-05-30 08:07:29 +0800

    fefactor: removed pipe tests

--- machines/qemu-virt-riscv64/applications/main.c
+++ machines/qemu-virt-riscv64/applications/main.c
@@ -9,8 +9,6 @@
 
 #include "dfs_file.h"
 #include "dfs_fs.h"
-#include "klibc/kstring.h"
-#include "sys/unistd.h"
 #include <rtthread.h>
 #include <rthw.h>
 #include <sys/stat.h>
@@ -26,28 +24,8 @@ int main(void)
     dfs_file_symlink("/musl/lib/libc.so", "/lib/ld-linux-riscv64-lp64.so.1");
     dfs_file_symlink("/dev/tty", "/dev/ttyS0");
 
-    int fildes[2];
-    pipe(fildes);
-    rt_kprintf("pipe fd: %d %d\n", fildes[0], fildes[1]);
-
-    char buf[1024];
-    rt_memset(buf, 'a', sizeof(buf));
-    buf[sizeof(buf) - 1] = '\0';
-    int ret = write(fildes[1], buf, sizeof(buf));
-    if (ret < 0) {
-        rt_kprintf("write failed: %d\n", ret);
-    } else {
-        rt_kprintf("write %d bytes: %s\n", ret, buf);
-    }
-    
-    ret = read(fildes[0], buf, sizeof(buf));
-    if (ret < 0) {
-        rt_kprintf("read failed: %d\n", ret);
-    } else {
-        rt_kprintf("read %d bytes: %s\n", ret, buf);
-    }
-
-    rt_kprintf("END of main\n");
+    mkdir("/bin", 0777);
+    dfs_file_symlink("/musl/busybox", "/bin/busybox");
 
     return 0;
 }
