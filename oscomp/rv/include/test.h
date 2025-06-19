f3c5abcebf2ba5bd0e9e2bed83528a0fd35930e8
You Zi <2746324475@qq.com>
2025-06-19 12:12:38 +0800

    feat: run musl tests

--- oscomp/rv/include/test.h
+++ oscomp/rv/include/test.h
@@ -1,8 +1,13 @@
 #ifndef _TEST_H_
 #define _TEST_H_
 
+void test_start_musl();
+
 int run_test(char *const argv[]);
 
-void test_busybox();
+void test_musl_basic();
+void test_musl_busybox();
+void test_musl_libc();
+void test_musl_lua();
 
 #endif
