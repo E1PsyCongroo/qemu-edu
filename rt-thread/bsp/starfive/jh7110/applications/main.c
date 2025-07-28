d221cafac6e2c941546e8eaabfc1fbab8f31a27a
hewo233 <2842137843@qq.com>
2025-07-28 16:35:43 +0800

    feat: add bsp starfive

---
+++ rt-thread/bsp/starfive/jh7110/applications/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#include <rtthread.h>
+#include <rthw.h>
+#include <stdio.h>
+#include <string.h>
+
+extern int rpmsg_test();
+int main(void)
+{
+    printf("Hello RISC-V\n");
+
+    return rpmsg_test();
+}
